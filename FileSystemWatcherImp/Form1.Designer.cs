5d43973917af796481e8ee283faddbb2ce7d5d7e
chintelin <chintelin@ncu.edu.tw>
2019-06-11 11:25:53 +0800

    The first implementation
    Features
    1. Monitor a user-selected folder
    2. Replace given strings separated with semicomma with a given string.
    3. Log history.
    
    This program is designed to modified NC programs for CNC machines. Because different CNCs may assign various M codes for the same work, i.e. M130 and M199, it is better to replace the M codes with Macro variable, such as M#222. Users can assign 130 and 199 to #222 in difference machines.

---
+++ FileSystemWatcherImp/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace FileSystemWatcherImp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.textBox_log = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.checkBox_Auto = new System.Windows.Forms.CheckBox();
+            this.textBox_ReplaceWith = new System.Windows.Forms.TextBox();
+            this.textBox_FindWhat = new System.Windows.Forms.TextBox();
+            this.textBox_TargetFolder = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Target Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Find what";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Replace with";
+            // 
+            // buttonSelectFolder
+            // 
+            this.buttonSelectFolder.Location = new System.Drawing.Point(419, 26);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectFolder.TabIndex = 4;
+            this.buttonSelectFolder.Text = "Select";
+            this.buttonSelectFolder.UseVisualStyleBackColor = true;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+            // 
+            // textBox_log
+            // 
+            this.textBox_log.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::FileSystemWatcherImp.Properties.Settings.Default, "History", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox_log.Location = new System.Drawing.Point(28, 164);
+            this.textBox_log.Multiline = true;
+            this.textBox_log.Name = "textBox_log";
+            this.textBox_log.Size = new System.Drawing.Size(466, 172);
+            this.textBox_log.TabIndex = 5;
+            this.textBox_log.Text = global::FileSystemWatcherImp.Properties.Settings.Default.History;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Systen Log";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(419, 80);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 50);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // checkBox_Auto
+            // 
+            this.checkBox_Auto.AutoSize = true;
+            this.checkBox_Auto.Checked = global::FileSystemWatcherImp.Properties.Settings.Default.EnableAuto;
+            this.checkBox_Auto.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::FileSystemWatcherImp.Properties.Settings.Default, "EnableAuto", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.checkBox_Auto.Location = new System.Drawing.Point(423, 136);
+            this.checkBox_Auto.Name = "checkBox_Auto";
+            this.checkBox_Auto.Size = new System.Drawing.Size(71, 16);
+            this.checkBox_Auto.TabIndex = 6;
+            this.checkBox_Auto.Text = "Auto Start";
+            this.checkBox_Auto.UseVisualStyleBackColor = true;
+            this.checkBox_Auto.CheckedChanged += new System.EventHandler(this.checkBox_Auto_CheckedChanged);
+            // 
+            // textBox_ReplaceWith
+            // 
+            this.textBox_ReplaceWith.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::FileSystemWatcherImp.Properties.Settings.Default, "newWord", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox_ReplaceWith.Location = new System.Drawing.Point(100, 108);
+            this.textBox_ReplaceWith.Name = "textBox_ReplaceWith";
+            this.textBox_ReplaceWith.Size = new System.Drawing.Size(300, 22);
+            this.textBox_ReplaceWith.TabIndex = 3;
+            this.textBox_ReplaceWith.Text = global::FileSystemWatcherImp.Properties.Settings.Default.newWord;
+            // 
+            // textBox_FindWhat
+            // 
+            this.textBox_FindWhat.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::FileSystemWatcherImp.Properties.Settings.Default, "FindWord", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox_FindWhat.Location = new System.Drawing.Point(100, 82);
+            this.textBox_FindWhat.Name = "textBox_FindWhat";
+            this.textBox_FindWhat.Size = new System.Drawing.Size(300, 22);
+            this.textBox_FindWhat.TabIndex = 3;
+            this.textBox_FindWhat.Text = global::FileSystemWatcherImp.Properties.Settings.Default.FindWord;
+            // 
+            // textBox_TargetFolder
+            // 
+            this.textBox_TargetFolder.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::FileSystemWatcherImp.Properties.Settings.Default, "targetFolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox_TargetFolder.Location = new System.Drawing.Point(100, 26);
+            this.textBox_TargetFolder.Multiline = true;
+            this.textBox_TargetFolder.Name = "textBox_TargetFolder";
+            this.textBox_TargetFolder.Size = new System.Drawing.Size(300, 50);
+            this.textBox_TargetFolder.TabIndex = 3;
+            this.textBox_TargetFolder.Text = global::FileSystemWatcherImp.Properties.Settings.Default.targetFolder;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 348);
+            this.Controls.Add(this.checkBox_Auto);
+            this.Controls.Add(this.textBox_log);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonSelectFolder);
+            this.Controls.Add(this.textBox_ReplaceWith);
+            this.Controls.Add(this.textBox_FindWhat);
+            this.Controls.Add(this.textBox_TargetFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_TargetFolder;
+        private System.Windows.Forms.Button buttonSelectFolder;
+        private System.Windows.Forms.TextBox textBox_FindWhat;
+        private System.Windows.Forms.TextBox textBox_ReplaceWith;
+        private System.Windows.Forms.TextBox textBox_log;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.CheckBox checkBox_Auto;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FileSystemWatcherImp/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;//namespace contains FileSystemWatcher
+
+namespace FileSystemWatcherImp
+{
+    public partial class Form1 : Form
+    {
+        internal class FileChangedArgs : EventArgs
+        {
+            public FileInfo ChangedFile { get; set; }
+        }
+
+        FileSystemWatcher watcher = new FileSystemWatcher()
+        {
+            IncludeSubdirectories = false,
+            NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName
+        };
+        StringBuilder logLines = new StringBuilder(2000);
+        int logCapacity = 1000;
+        int logLinesLength = 0;
+        EventHandler<FileChangedArgs> FileChanged;
+        bool isFileModified = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            watcher.Created += new FileSystemEventHandler(watcher_Detected);
+            watcher.Changed += new FileSystemEventHandler(watcher_Detected);
+            watcher.Renamed += new RenamedEventHandler(watcher_Renamed);
+
+            FileChanged += FileChanged_Action;
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            logLines.Append(Properties.Settings.Default.History);
+            checkBox_Auto.Checked = Properties.Settings.Default.EnableAuto;
+            if (checkBox_Auto.Checked)
+                checkBox_Auto_CheckedChanged(null, null);
+        }
+
+        private void buttonSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var path = fbd.SelectedPath;
+                if (path != null)
+                {
+                    textBox_TargetFolder.Text = path;
+                    Properties.Settings.Default.Save();//remember the setting
+                }
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {            
+            watcher.Path = textBox_TargetFolder.Text;
+            watcher.EnableRaisingEvents = true; //start watching
+
+            timer1.Start();
+            buttonStart.Enabled = false;
+            string logTextLine = string.Format("{0} Watcher is detecting...{1}{2}", DateTime.Now, Environment.NewLine, Environment.NewLine);
+            logLines.Insert(0, logTextLine);
+                     
+        }
+
+        void watcher_Renamed(object sender, RenamedEventArgs e)
+        {            
+            watcher.EnableRaisingEvents = false;
+            string logTextLine = string.Format("{0} {1}: {2} -> {3}.{4}", DateTime.Now, e.ChangeType, e.OldName, e.Name, Environment.NewLine);
+            logLines.Insert(0, logTextLine);
+            watcher.EnableRaisingEvents = true;
+        }
+
+
+        void watcher_Detected(object sender, FileSystemEventArgs e)
+        {           
+            watcher.EnableRaisingEvents = false;
+            string logTextLine = string.Format("{0} {1}: {2}.{3}", DateTime.Now, e.ChangeType, e.Name, Environment.NewLine);
+            logLines.Insert(0, logTextLine);
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                FileChangedArgs fca = new FileChangedArgs();
+                fca.ChangedFile = new FileInfo(e.FullPath);
+
+                
+                if (FileChanged != null)
+                {
+                    var fileChangedEvent = new EventHandler<FileChangedArgs>(FileChanged);
+                    fileChangedEvent(null, fca);
+                }
+            }
+            watcher.EnableRaisingEvents = true;
+        }
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (logLines.Length == logLinesLength)
+                return;
+
+            timer1.Stop();
+            {               
+                if (logLines.Length > logCapacity)
+                {
+                    int startIndex = Convert.ToInt32(logCapacity * 0.8);
+                    int removeCount = Convert.ToInt32(logLines.Length - logCapacity * 0.8);
+                    logLines.Remove(startIndex, removeCount);
+                }
+
+                logLinesLength = logLines.Length;
+                textBox_log.Text = logLines.ToString();
+
+                Properties.Settings.Default.Save();//remember the setting
+            }
+            timer1.Start();
+        }
+
+
+
+        private void checkBox_Auto_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();//remember the setting
+            if (checkBox_Auto.Checked)
+            {
+                buttonStart.Enabled = false;
+                buttonStart_Click(null, null);
+            }
+            else
+            {
+                buttonStart.Enabled = true;
+            }
+        }
+
+        private void FileChanged_Action(object s, FileChangedArgs f)
+        {
+            bool watchStatus = watcher.EnableRaisingEvents;
+            try
+            {
+                string findStrRaw = textBox_FindWhat.Text;
+                string[] findStrings = findStrRaw.Replace(" ", "").Split(';');                
+                string newStr = textBox_ReplaceWith.Text;
+
+                if (findStrings == null || newStr == null)
+                {
+                    logLines.Insert(0, DateTime.Now.ToString() + "Cannot replace any words");
+                    return;
+                }
+
+                string content;
+                using (StreamReader sr = new StreamReader(f.ChangedFile.FullName))
+                {
+                    content = sr.ReadToEnd();  
+                }
+
+                foreach (var findStr in findStrings)
+                {
+                    content = content.Replace(findStr, newStr);
+                }
+
+                isFileModified = true;
+                watcher.EnableRaisingEvents = false;
+                lock (watcher)
+                {
+                    using (StreamWriter sw = new StreamWriter(f.ChangedFile.FullName))
+                    {
+                        sw.Write(content);
+                    }
+                    logLines.Insert(0, DateTime.Now.ToString() + " Data is modified" + Environment.NewLine);
+                }
+            }
+            catch (Exception e)
+            {
+                logLines.Insert(0, DateTime.Now.ToString() + " **ERROR** " + e.ToString() + Environment.NewLine);
+                return;
+            }
+            finally
+            {
+                isFileModified = false;
+                watcher.EnableRaisingEvents = watchStatus;
+            }
+        }
+    }
+}
